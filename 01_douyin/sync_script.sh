696c181a3ed667288747a464c881154108147e0f
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2023-07-31 19:19:09 +0800

    Update sync_script.sh

--- 01_douyin/sync_script.sh
+++ 01_douyin/sync_script.sh
@@ -2,7 +2,7 @@
 
 # 定义源目录和目标服务器信息
 source_dir="/home/01_html/02_douyVideo/"
-target_server="root@128.199.14.4"
+target_server="root@127.199.13.2"
 target_dir="/home/01_html/02_douyVideo/"
 
 # 执行rsync命令，添加-e选项指定使用SSH密钥认证
